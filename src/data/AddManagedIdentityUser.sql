9a8f4ebf7e4860b61bdbd845dbcab52bdcdf8940
Raffi <56635140+raffialqurran@users.noreply.github.com>
2025-01-28 11:52:57 +0000

    modified:   src/ContosoSuitesWebAPI/Program.cs 	modified:   src/ContosoSuitesWebAPI/appsettings.json 	modified:   src/data/AddManagedIdentityUser.sql

--- src/data/AddManagedIdentityUser.sql
+++ src/data/AddManagedIdentityUser.sql
@@ -1,4 +1,4 @@
 CREATE USER TechExcelMI FROM EXTERNAL PROVIDER;
-ALTER ROLE db_datareader ADD MEMBER TechExcelMI;
-ALTER ROLE db_datawriter ADD MEMBER TechExcelMI;
+ALTER ROLE db_datareader ADD MEMBER id-4k5wior5cxlxm;
+ALTER ROLE db_datawriter ADD MEMBER id-4k5wior5cxlxm;
 
